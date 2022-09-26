49177cc8252698521a871bb341056e70bef6f3eb
GustavoGiga123 <110988741+GustavoGiga123@users.noreply.github.com>
2022-09-26 13:15:48 +0000

    aaaa

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,17 +1,33 @@
 <template>
-  <section class="section">
-    <h2 class="title is-3 has-text-grey">
-      "Just start  <b-icon
-        icon="rocket"
-        size="is-large"
-      />"
-    </h2>
-    <h3 class="subtitle is-6 has-text-grey">
-      Author: <a href="https://github.com/anteriovieira">
-        Antério Vieira
-      </a>
-    </h3>
-  </section>
+    <div>
+        <section class="hero is-dark">
+            <div class="hero-body">
+                <p class="title">
+                    Mestres do trava Zap - Ferramentas e Técticas
+                </p>
+                <p class="subtitle">
+                    CURSO COMPLETO - TRAVA ZAP
+                </p>
+            </div>
+        </section>  
+
+      <section class="section mt-3">
+        <h1 class="title"><strong>O que você aprenderá:</strong></h1>
+    <h1 class= "text">* Tudo que você precisa saber para começar a travar o zap de seus amigos </h1>
+        <h1 class= "text">* Exemplos detalhados - Zap de famosos travado por 1 semana, tudo disponível em vídeo  </h1>
+        <a href="https://bulma.io/documentation/layout/hero/" target="_blank">Documentação Hero</a>
+      </section>
+
+      <section class="section mt-3">
+        <h1 class="title"><strong>Este curso Inclui:</strong></h1>
+    <h1 class= "text">* 50 horas de pura imersão </h1>
+        <h1 class= "text">* Acesso por qualquer dispositivo  </h1>
+        <a href="https://bulma.io/documentation/layout/hero/" target="_blank">Documentação Hero</a>
+      </section>
+    
+    </div>
+
+  
 </template>
 
 <script>
