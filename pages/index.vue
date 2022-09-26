e60bedb82e290b7bf6c96a73f4cd43bdfb16b087
GustavoGiga123 <110988741+GustavoGiga123@users.noreply.github.com>
2022-09-26 12:46:48 +0000

    jgnf

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,91 @@
 <template>
-  <section class="section">
-    <h1 class="title">Página inicial</h1>
+  
+  <section>
+    <h1 class="title"><strong>Uma ampla gama de cursos para você!</strong></h1>
+    <h1 class= "subtitle">Mais de 1 milhão de cursos sobre como vender cursos, professores nativos, pack do pezinho e muito mais</h1>
+
+    <div>
+
+    <section>
+      <b-tabs>
+        <b-tab-item label="Trava Zap"> 
+            <h1 class="subtitle is-5"><strong>Amplie suas oportunidades de carreira com aulas de golpe do Zap</strong></h1>
+        Se você trabalha com mercenários ou tias da cantina ou está buscando uma carreira como mecânico da Carfix, o trava Zaps é umas das habilidades mais importantes que você pode aprender! A sintaxe simples do Zap é especialmente adequada para apps de Desktop, web e negócios.
+        </b-tab-item>
+
+        <b-tab-item label="Python">
+          Lorem <br />
+          ipsum <br />
+          dolor <br />
+          sit <br />
+          amet.
+        </b-tab-item>
+      </b-tabs>
+    </section>
+
+  </div>
+    
+    <b-button tag="router-link" :to="{ path: '/sobre' }" type="is-dark ml-3">Descubra outras categorias</b-button>
+    
+      <section class="section">
+  <div class="columns">
+  <div class="column">
+    <img 
+      src="https://static-media.hotmart.com/qH_uTqGXwCQ9qa2-e3NbD4QHJXE=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/6a6d27d0-8776-4ab2-ba57-a525ff770f94/dolado.png?w=920"></img>
+    <p class="subtitle">Mestres do trava Zap</p>
+    R$199,90
+    <section>
+        <b-rate custom-text=""></b-rate>
+        <b-rate 
+            icon-pack="fas" 
+            @change="success" 
+            </b-rate>
+    </section>
+  </div>
+  <div class="column">
+    <img 
+      src="https://static-media.hotmart.com/5MTGoAQ5S8Tk4eg8L0sqAdAHX0M=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/279fdb42-7df2-426b-93b1-ba948e158840/hotmartcapaproduto02PNG.png?w=920">
+   <p class="subtitle">Como meditar</p>
+    R$599,90
+    <section>
+        <b-rate custom-text=""></b-rate>
+        <b-rate 
+            icon-pack="fas" 
+            @change="success" 
+            </b-rate>
+    </section>
+  </div>
+  <div class="column">
+    <img 
+      src="https://static-media.hotmart.com/cgz-5S0kkZD4eLEKCoFkVFZWUGQ=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_contents/4c1b125f-201c-4bb5-9539-063fcea5ef14/Logo7.jpg?w=920">
+   <p class="subtitle">Seja um agiota</p>
+    R$199,90
+    <section>
+        <b-rate custom-text=""></b-rate>
+        <b-rate 
+            icon-pack="fas" 
+            @change="success" 
+            </b-rate>
+    </section>
+  </div>
+  <div class="column">
+    <img 
+      src="https://static-media.hotmart.com/4N6F_QovPUw8oDr7CzIFvNuZmUY=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/722376df-e637-4643-ad44-75af9a98822a/capa52012.png?w=920">
+   <p class="subtitle">Curso de maquiagem</p>
+    R$1099,90
+    <section>
+        <b-rate custom-text=""></b-rate>
+        <b-rate 
+            icon-pack="fas" 
+            @change="success" 
+            </b-rate>
+    </section>
+  </div>
+</div>
+      </section>
   </section>
+  
+
+  
 </template>
 
--- pages/sobre.vue
+++ pages/sobre.vue
@@ -1,11 +1,73 @@
 <template>
-  <section class="section">
-    <h1 class="title">Sobre</h1>
-  </section>
-</template>
+    <section class="section">
+        <h1 class="title">Principais categorias</h1>
 
-<script>
-export default {
-  name: 'InspirePage'
-}
-</script>
+       
+
+        <div class="columns mt-4 box">
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/4XPH_wqMavvOQsxu0JEZDUnAc0w=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_contents/67be9d12-72ac-460b-aa88-038814baff95/Restmoretodayreadyourfavoritebookdrinkwarmteaeverythingelsetomorrow.png?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Finanças</h5>
+
+            </div>
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/ATWOLWvRh_NfyVuEJthPzCvBGy0=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_contents/1e6d0510-d771-4b11-85a5-173e903ee772/programacaoparaintermediarios.jpg?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Programação</h5>
+
+            </div>
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/EsGhP_Y-ZxNwH3bsgTfDSiqbun8=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_contents/87af86be-12e1-4779-80b9-80104675f89b/designsimplificadolaranja.png?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Design</h5>
+
+            </div>
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/OSJG1BiqoH8NRXHWWmT4AUlWGRw=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/59516099-2efe-4431-830e-46f2d90e462a/Designsemnome7.png?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Marketing</h5>
+
+            </div>
+        </div>
+
+        <div class="columns mt-4">
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/z7Bt3rEH7lZ5LQtWdwq2Pb0_Ihg=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/12bde925-95b9-46ce-9ce4-2d6e144b1729/capa52010.png?w=9208">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Fotografia</h5>
+
+            </div>
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/UeAsiv2WFAfNMZxKkYjUYkYF1GQ=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_contents/2bb9dc1c-43b5-43d9-bab3-42530579c201/165440038_2581198872173331_6340738996083384695_n.jpg?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Maquiagem</h5>
+
+            </div>
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/NXvhBaqOoiriuSS6VH9v-iIUNIw=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/5276bdc9-c228-45b3-a0ac-94a57543dac5/THUMBPerfildeNegcios1.png?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Negócios</h5>
+
+            </div>
+            <div class="column">
+                <figure class="image is-200x200">
+                    <img src="https://static-media.hotmart.com/5IBBzswQPZw_0g5RVRNPA0_kLYU=/300x300/smart/filters:format(webp):background_color(white)/hotmart/product_pictures/58bc8642-64a7-47d3-8b75-232918a14121/LOGOATUALIZADO.jpg?w=920">
+                </figure>
+                <h5 class="title is-5 mt-4 has-text-centered">Música</h5>
+
+            </div>
+        </div>
+
+
+
+
+    </section>
+</template>
